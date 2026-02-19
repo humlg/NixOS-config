ebc6b2c96aba277a96c530e4218f638802a5b2e1
David Huml <david.huml@email.cz>
2026-02-19 17:25:20 +0100

    Moved hw config file and configuration file of VM to it's host dir

---
+++ hosts/sauron-nixos/configuration.nix
@@ -0,0 +1,328 @@
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+ 
+{ config, lib, pkgs, inputs, ... }:
+ 
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      #./modules/desktop/hyprland.nix
+    ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    #driSupport = true;
+    #driSupport32Bit = true;
+  };
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false; # RTX 2060 → use proprietary driver
+    nvidiaSettings = true;
+  };
+
+  #hardware.enableRedustributedFirmware = true;
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Mediam,Socket";
+      Experimental = true;
+    };
+  };
+  
+  boot.kernelParams = ["nvidia_drm.modeset=1"];
+  services.xserver.videoDrivers = ["nvidia"];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+ 
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  boot.kernelModules = [ "nct6775" ];
+ 
+  networking.hostName = "sauron"; # Define your hostname.
+ 
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+ 
+  # Set your time zone.
+  time.timeZone = "Europe/Prague";
+ 
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+ 
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # Fine-grained locale settings
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS        = "cs_CZ.UTF-8";
+    LC_IDENTIFICATION = "cs_CZ.UTF-8";
+    LC_MEASUREMENT    = "cs_CZ.UTF-8"; # metric units, Celsius
+    LC_MONETARY       = "cs_CZ.UTF-8";
+    LC_NAME           = "cs_CZ.UTF-8";
+    LC_NUMERIC        = "cs_CZ.UTF-8";
+    LC_PAPER          = "cs_CZ.UTF-8";
+    LC_TELEPHONE      = "cs_CZ.UTF-8";
+    LC_TIME           = "cs_CZ.UTF-8"; # DD.MM.YYYY, 24h clock
+  };  
+ console = {
+    font = "Lat2-Terminus16";
+    keyMap = "cz";
+ };
+
+  services.udisks2.enable = true;
+  fileSystems."/mnt/data1" = {
+    device = "/dev/disk/by-uuid/c424be95-313a-4251-b702-587ff795c7f9";
+    fsType = "ext4";
+    options = ["nofail" "x-systemd.automatic"];
+  };
+  fileSystems."/home/david/Documents" = {
+    device = "/mnt/data1/Documents";
+    options = [ "bind" ];
+  };
+
+  fileSystems."/home/david/Downloads" = {
+    device = "/mnt/data1/Downloads";
+    options = [ "bind" ];
+  };
+
+  fileSystems."/home/david/Pictures" = {
+    device = "/mnt/data1/Pictures";
+    options = [ "bind" ];
+  };
+
+  fileSystems."/home/david/Music" = {
+    device = "/mnt/data1/Music";
+    options = [ "bind" ];
+  };
+
+  fileSystems."/home/david/Videos" = {
+    device = "/mnt/data1/Videos";
+    options = [ "bind" ];
+  };
+
+
+  fileSystems."/mnt/data2" = {
+     device = "/dev/disk/by-uuid/085a2044-a0fe-4b57-9beb-2fa4f747e416";
+     fsType = "ext4";
+     options = ["nofail" "x-systemd.automatic"];
+  };
+
+
+   services.displayManager.sddm = {
+      enable = true;
+      wayland.enable = true;
+   };
+   services.displayManager.sddm.theme = "sugar-dark";
+ 
+   programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+      xwayland.enable = true;
+  };
+ 
+  security.polkit.enable = true;
+ 
+  systemd.user.services.polkit-agent = {
+    description = "Polkit Authentication Agent";
+    wantedBy = [ "default.target" ];
+ 
+      serviceConfig = {
+        ExecStart = "${pkgs.lxqt.lxqt-policykit}/bin/lxqt-policykit-agent";
+        Restart = "on-failure";
+      };
+    };
+
+  environment.variables = {
+	NIXPKGS_ALLOW_UNFREE = 1;
+  };
+  
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+
+    XCURSOR_THEME = "volantes_cursors";
+    XCURSOR_SIZE = "48";
+
+    KITTY_DISABLE_WAYLAND = "1";
+  };
+
+ 
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+ 
+  # Enable CUPS to print documents.
+   services.printing.enable = true;
+ 
+   services.colord.enable = true;
+
+   #services.lm_sensors.enable = true;
+
+  # Enable sound.
+   services.pipewire = {
+     enable = true;
+     pulse.enable = true;
+   };
+ 
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+   services.libinput.enable = true;
+ 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.david = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+     shell = pkgs.zsh;
+     packages = with pkgs; [
+       tree
+     ];
+   };
+   users.users.root.shell = pkgs.zsh;
+
+   home-manager = {
+    extraSpecialArgs = { inherit inputs;};
+    users = {
+      "david" = import ../../home.nix;
+    };
+   };
+ 
+   programs.firefox.enable = true;
+   programs.thunar.enable = true;
+   programs.zsh = {
+	enable = true;
+	autosuggestions.enable = true;
+	syntaxHighlighting.enable = true;
+	ohMyZsh = {
+		enable = true;
+		plugins = ["git"];
+		theme = "agnoster";
+	};
+	interactiveShellInit = ''
+		if command -v fastfetch >/dev/null 2>&1; then
+			fastfetch
+		fi
+		'';
+   };
+   programs.kdeconnect.enable = true;
+   programs.coolercontrol.enable = true;
+ 
+  # Enable non-free nix packages
+  nixpkgs.config.allowUnfree = true;
+ 
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+   environment.systemPackages = with pkgs; [
+	vim
+	wget
+	htop
+	btop
+	kitty
+	gtk3
+	xterm
+	alacritty
+	rofi
+	wofi
+	waybar
+	git
+	unzip
+	pywal16
+	waypaper
+	chromium
+	swww
+	curl
+	wget
+	spice-vdagent
+	volantes-cursors
+	fastfetch
+	discord
+	vscode
+	lxqt.lxqt-policykit
+	steam
+  kdePackages.kate
+	mpv
+	ffmpeg
+	pywalfox-native
+	sddm-sugar-dark
+	quickshell
+	pavucontrol
+	kdePackages.kdeconnect-kde
+	cliphist
+	wl-clipboard
+	hyprlock
+	hypridle
+	lm_sensors
+	stress
+	cava
+	openrgb
+	hyprshot
+	swaynotificationcenter
+	obsidian
+	usbutils
+	blueman
+	gamescope
+	udiskie
+	librewolf
+	tree
+  spotify
+  ];
+ 
+ 
+   fonts.packages = with pkgs; [
+   nerd-fonts.jetbrains-mono
+   ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+ 
+  # List services that you want to enable:
+ 
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+ 
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+ 
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+ 
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+ 
+}
