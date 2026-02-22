f7acc7d91d8cec1cd8f49b3edac11c041420d4ff
David Huml <david.huml@email.cz>
2026-02-22 02:34:04 +0100

    Started dark theme module

---
+++ modules/desktop/dark-theme.nix
@@ -0,0 +1,73 @@
+{ config, lib, pkgs, ... }:
+
+let
+  gtkThemeName = "Orchis-Purple-Dark-Compact";
+in
+{
+  programs.dconf.enable = true;
+
+  # Nudge apps/toolkits system-wide
+  environment.sessionVariables = {
+    GTK_THEME = gtkThemeName;
+
+    QT_QPA_PLATFORMTHEME = "qt6ct";
+    QT_QPA_PLATFORMTHEME_QT5 = "qt5ct";
+    QT_STYLE_OVERRIDE = "kvantum";
+  };
+
+  # Apply to all Home Manager users
+  home-manager.sharedModules = [
+    ({ pkgs, ... }: {
+      home.packages = with pkgs; [
+        orchis-theme
+        adwaita-icon-theme
+
+        qt6ct qt5ct
+        kvantum
+      ];
+
+      gtk = {
+        enable = true;
+
+        theme = {
+          name = gtkThemeName;
+          package = pkgs.orchis-theme;
+        };
+
+        iconTheme = {
+          name = "Adwaita";
+          package = pkgs.adwaita-icon-theme;
+        };
+
+        gtk3.extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
+
+        gtk4.extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
+      };
+
+      dconf.settings = {
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          gtk-theme = gtkThemeName;
+          icon-theme = "Adwaita";
+        };
+      };
+
+      qt = {
+        enable = true;
+        platformTheme.name = "qtct";
+        style.name = "kvantum";
+      };
+
+      home.sessionVariables = {
+        GTK_THEME = gtkThemeName;
+        QT_QPA_PLATFORMTHEME = "qt6ct";
+        QT_QPA_PLATFORMTHEME_QT5 = "qt5ct";
+        QT_STYLE_OVERRIDE = "kvantum";
+      };
+    })
+  ];
+}
