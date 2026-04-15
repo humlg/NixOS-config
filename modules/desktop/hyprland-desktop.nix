d64e58eb6b0dca08221dbb75adf3b7c688a549d6
David Huml <david.huml@email.cz>
2026-04-15 10:25:31 +0200

    Fix wallust template syntax: use single braces for variables
    Wallust's template engine uses {variable} (single braces) for
    substitution, not {{variable}} (which are escape sequences for literal
    braces). Also rewrote colors.json template as plain text with {{ / }}
    escapes for JSON structural braces.

--- modules/desktop/hyprland-desktop.nix
+++ modules/desktop/hyprland-desktop.nix
@@ -264,178 +264,180 @@ WAYPAPEREOF
     '';
 
     home.file.".config/wallust/templates/colors-hyprland.conf".text = ''
-      $background = rgb({{background.strip}})
-      $foreground = rgb({{foreground.strip}})
-      $color0 = rgb({{color0.strip}})
-      $color1 = rgb({{color1.strip}})
-      $color2 = rgb({{color2.strip}})
-      $color3 = rgb({{color3.strip}})
-      $color4 = rgb({{color4.strip}})
-      $color5 = rgb({{color5.strip}})
-      $color6 = rgb({{color6.strip}})
-      $color7 = rgb({{color7.strip}})
-      $color8 = rgb({{color8.strip}})
-      $color9 = rgb({{color9.strip}})
-      $color10 = rgb({{color10.strip}})
-      $color11 = rgb({{color11.strip}})
-      $color12 = rgb({{color12.strip}})
-      $color13 = rgb({{color13.strip}})
-      $color14 = rgb({{color14.strip}})
-      $color15 = rgb({{color15.strip}})
-      $wallpaper = {{wallpaper}}
+      $background = rgb({background.strip})
+      $foreground = rgb({foreground.strip})
+      $color0 = rgb({color0.strip})
+      $color1 = rgb({color1.strip})
+      $color2 = rgb({color2.strip})
+      $color3 = rgb({color3.strip})
+      $color4 = rgb({color4.strip})
+      $color5 = rgb({color5.strip})
+      $color6 = rgb({color6.strip})
+      $color7 = rgb({color7.strip})
+      $color8 = rgb({color8.strip})
+      $color9 = rgb({color9.strip})
+      $color10 = rgb({color10.strip})
+      $color11 = rgb({color11.strip})
+      $color12 = rgb({color12.strip})
+      $color13 = rgb({color13.strip})
+      $color14 = rgb({color14.strip})
+      $color15 = rgb({color15.strip})
+      $wallpaper = {wallpaper}
     '';
 
     home.file.".config/wallust/templates/colors-kitty.conf".text = ''
-      foreground {{foreground}}
-      background {{background}}
-      cursor     {{foreground}}
-      color0  {{color0}}
-      color1  {{color1}}
-      color2  {{color2}}
-      color3  {{color3}}
-      color4  {{color4}}
-      color5  {{color5}}
-      color6  {{color6}}
-      color7  {{color7}}
-      color8  {{color8}}
-      color9  {{color9}}
-      color10 {{color10}}
-      color11 {{color11}}
-      color12 {{color12}}
-      color13 {{color13}}
-      color14 {{color14}}
-      color15 {{color15}}
+      foreground {foreground}
+      background {background}
+      cursor     {foreground}
+      color0  {color0}
+      color1  {color1}
+      color2  {color2}
+      color3  {color3}
+      color4  {color4}
+      color5  {color5}
+      color6  {color6}
+      color7  {color7}
+      color8  {color8}
+      color9  {color9}
+      color10 {color10}
+      color11 {color11}
+      color12 {color12}
+      color13 {color13}
+      color14 {color14}
+      color15 {color15}
     '';
 
     home.file.".config/wallust/templates/colors-waybar.css".text = ''
-      @define-color background {{background}};
-      @define-color foreground {{foreground}};
-      @define-color color0  {{color0}};
-      @define-color color1  {{color1}};
-      @define-color color2  {{color2}};
-      @define-color color3  {{color3}};
-      @define-color color4  {{color4}};
-      @define-color color5  {{color5}};
-      @define-color color6  {{color6}};
-      @define-color color7  {{color7}};
-      @define-color color8  {{color8}};
-      @define-color color9  {{color9}};
-      @define-color color10 {{color10}};
-      @define-color color11 {{color11}};
-      @define-color color12 {{color12}};
-      @define-color color13 {{color13}};
-      @define-color color14 {{color14}};
-      @define-color color15 {{color15}};
+      @define-color background {background};
+      @define-color foreground {foreground};
+      @define-color color0  {color0};
+      @define-color color1  {color1};
+      @define-color color2  {color2};
+      @define-color color3  {color3};
+      @define-color color4  {color4};
+      @define-color color5  {color5};
+      @define-color color6  {color6};
+      @define-color color7  {color7};
+      @define-color color8  {color8};
+      @define-color color9  {color9};
+      @define-color color10 {color10};
+      @define-color color11 {color11};
+      @define-color color12 {color12};
+      @define-color color13 {color13};
+      @define-color color14 {color14};
+      @define-color color15 {color15};
     '';
 
     home.file.".config/wallust/templates/colors-rofi-dark.rasi".text = ''
       * {{
-          background:     {{background}};
-          foreground:     {{foreground}};
-          active-bg:      {{color2}};
-          urgent-bg:      {{color1}};
-          selected-bg:    {{color2}};
-          selected-fg:    {{foreground}};
+          background:     {background};
+          foreground:     {foreground};
+          active-bg:      {color2};
+          urgent-bg:      {color1};
+          selected-bg:    {color2};
+          selected-fg:    {foreground};
       }}
     '';
 
-    home.file.".config/wallust/templates/colors.json".text = builtins.toJSON {
-      special = {
-        background  = "{{background}}";
-        foreground  = "{{foreground}}";
-        cursor      = "{{foreground}}";
-      };
-      colors = {
-        color0  = "{{color0}}";
-        color1  = "{{color1}}";
-        color2  = "{{color2}}";
-        color3  = "{{color3}}";
-        color4  = "{{color4}}";
-        color5  = "{{color5}}";
-        color6  = "{{color6}}";
-        color7  = "{{color7}}";
-        color8  = "{{color8}}";
-        color9  = "{{color9}}";
-        color10 = "{{color10}}";
-        color11 = "{{color11}}";
-        color12 = "{{color12}}";
-        color13 = "{{color13}}";
-        color14 = "{{color14}}";
-        color15 = "{{color15}}";
-      };
-    };
+    home.file.".config/wallust/templates/colors.json".text = ''
+      {{
+        "special": {{
+          "background": "{background}",
+          "foreground": "{foreground}",
+          "cursor": "{foreground}"
+        }},
+        "colors": {{
+          "color0": "{color0}",
+          "color1": "{color1}",
+          "color2": "{color2}",
+          "color3": "{color3}",
+          "color4": "{color4}",
+          "color5": "{color5}",
+          "color6": "{color6}",
+          "color7": "{color7}",
+          "color8": "{color8}",
+          "color9": "{color9}",
+          "color10": "{color10}",
+          "color11": "{color11}",
+          "color12": "{color12}",
+          "color13": "{color13}",
+          "color14": "{color14}",
+          "color15": "{color15}"
+        }}
+      }}
+    '';
 
     home.file.".config/wallust/templates/colors-cava".text = ''
       [color]
       gradient = 0
       gradient_count = 0
-      foreground = '{{foreground}}'
-      color1 = '{{color1}}'
-      color2 = '{{color2}}'
-      color3 = '{{color3}}'
-      color4 = '{{color4}}'
-      color5 = '{{color5}}'
-      color6 = '{{color6}}'
-      color7 = '{{color7}}'
+      foreground = '{foreground}'
+      color1 = '{color1}'
+      color2 = '{color2}'
+      color3 = '{color3}'
+      color4 = '{color4}'
+      color5 = '{color5}'
+      color6 = '{color6}'
+      color7 = '{color7}'
     '';
 
     home.file.".config/wallust/templates/colors-btop".text = ''
-      theme[main_bg]="{{background}}"
-      theme[main_fg]="{{foreground}}"
-      theme[title]="{{foreground}}"
-      theme[hi_fg]="{{color2}}"
-      theme[selected_bg]="{{color1}}"
-      theme[selected_fg]="{{foreground}}"
-      theme[inactive_fg]="{{color8}}"
-      theme[proc_misc]="{{color6}}"
-      theme[cpu_box]="{{color2}}"
-      theme[mem_box]="{{color3}}"
-      theme[net_box]="{{color4}}"
-      theme[proc_box]="{{color5}}"
-      theme[div_line]="{{color8}}"
-      theme[temp_start]="{{color2}}"
-      theme[temp_mid]="{{color3}}"
-      theme[temp_end]="{{color1}}"
-      theme[cpu_start]="{{color2}}"
-      theme[cpu_mid]="{{color4}}"
-      theme[cpu_end]="{{color1}}"
-      theme[free_start]="{{color2}}"
-      theme[free_mid]="{{color4}}"
-      theme[free_end]="{{color6}}"
-      theme[cached_start]="{{color6}}"
-      theme[cached_mid]="{{color4}}"
-      theme[cached_end]="{{color2}}"
-      theme[available_start]="{{color3}}"
-      theme[available_mid]="{{color2}}"
-      theme[available_end]="{{color6}}"
-      theme[used_start]="{{color1}}"
-      theme[used_mid]="{{color3}}"
-      theme[used_end]="{{color2}}"
-      theme[download_start]="{{color4}}"
-      theme[download_mid]="{{color2}}"
-      theme[download_end]="{{color6}}"
-      theme[upload_start]="{{color1}}"
-      theme[upload_mid]="{{color3}}"
-      theme[upload_end]="{{color2}}"
+      theme[main_bg]="{background}"
+      theme[main_fg]="{foreground}"
+      theme[title]="{foreground}"
+      theme[hi_fg]="{color2}"
+      theme[selected_bg]="{color1}"
+      theme[selected_fg]="{foreground}"
+      theme[inactive_fg]="{color8}"
+      theme[proc_misc]="{color6}"
+      theme[cpu_box]="{color2}"
+      theme[mem_box]="{color3}"
+      theme[net_box]="{color4}"
+      theme[proc_box]="{color5}"
+      theme[div_line]="{color8}"
+      theme[temp_start]="{color2}"
+      theme[temp_mid]="{color3}"
+      theme[temp_end]="{color1}"
+      theme[cpu_start]="{color2}"
+      theme[cpu_mid]="{color4}"
+      theme[cpu_end]="{color1}"
+      theme[free_start]="{color2}"
+      theme[free_mid]="{color4}"
+      theme[free_end]="{color6}"
+      theme[cached_start]="{color6}"
+      theme[cached_mid]="{color4}"
+      theme[cached_end]="{color2}"
+      theme[available_start]="{color3}"
+      theme[available_mid]="{color2}"
+      theme[available_end]="{color6}"
+      theme[used_start]="{color1}"
+      theme[used_mid]="{color3}"
+      theme[used_end]="{color2}"
+      theme[download_start]="{color4}"
+      theme[download_mid]="{color2}"
+      theme[download_end]="{color6}"
+      theme[upload_start]="{color1}"
+      theme[upload_mid]="{color3}"
+      theme[upload_end]="{color2}"
     '';
 
     home.file.".config/wallust/templates/colors-wal.vim".text = ''
-      highlight Normal     ctermbg=NONE guibg={{background}} guifg={{foreground}}
-      highlight Comment    guifg={{color8}}
-      highlight Constant   guifg={{color3}}
-      highlight Identifier guifg={{color4}}
-      highlight Statement  guifg={{color1}}
-      highlight PreProc    guifg={{color5}}
-      highlight Type       guifg={{color6}}
-      highlight Special    guifg={{color2}}
-      highlight Underlined guifg={{color4}} gui=underline
-      highlight Error      guifg={{color1}} guibg={{background}}
-      highlight Todo       guifg={{color3}} guibg={{background}} gui=bold
-      highlight LineNr     guifg={{color8}}
-      highlight CursorLine guibg={{color0}}
-      highlight StatusLine guibg={{color1}} guifg={{foreground}}
-      highlight Pmenu      guibg={{color0}} guifg={{foreground}}
-      highlight PmenuSel   guibg={{color2}} guifg={{background}}
+      highlight Normal     ctermbg=NONE guibg={background} guifg={foreground}
+      highlight Comment    guifg={color8}
+      highlight Constant   guifg={color3}
+      highlight Identifier guifg={color4}
+      highlight Statement  guifg={color1}
+      highlight PreProc    guifg={color5}
+      highlight Type       guifg={color6}
+      highlight Special    guifg={color2}
+      highlight Underlined guifg={color4} gui=underline
+      highlight Error      guifg={color1} guibg={background}
+      highlight Todo       guifg={color3} guibg={background} gui=bold
+      highlight LineNr     guifg={color8}
+      highlight CursorLine guibg={color0}
+      highlight StatusLine guibg={color1} guifg={foreground}
+      highlight Pmenu      guibg={color0} guifg={foreground}
+      highlight PmenuSel   guibg={color2} guifg={background}
       set background=dark
     '';
   };
