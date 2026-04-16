e3ea2ca71f8dbf0b9061561e72af0940ee11e146
David Huml <david.huml@email.cz>
2026-04-16 02:09:21 +0200

    Remove border, shadow, animation, and rounding from Albert window

--- modules/desktop/hyprland-config/window-rules.nix
+++ modules/desktop/hyprland-config/window-rules.nix
@@ -4,6 +4,8 @@
   windowrule = workspace special:mail silent,  match:initial_class ^(thunderbird)$
   windowrule = workspace special:notes silent, match:initial_class ^(obsidian)$
 
+  windowrule = match:initial_class ^(albert)$, noborder on, noshadow on, noanim on, norounding on
+
   windowrule = match:title File Operation Progress,   float on, center on
   windowrule = match:initial_title ^Write:.*,         float on, center on
   windowrule = match:initial_title Calendar Reminders, float on, center on
