bbaadff6c990f9bfdea0911ef15defb96d1d6bc2
David Huml <david.huml@email.cz>
2026-04-20 12:24:51 +0200

    Restore wallpaper on startup via waypaper --restore after swww-daemon

--- modules/desktop/hyprland-config/autostart.nix
+++ modules/desktop/hyprland-config/autostart.nix
@@ -4,6 +4,7 @@
   exec-once = thunar --daemon
   exec-once = ags run
   exec-once = swww-daemon
+  exec-once = sleep 1 && waypaper --restore
   exec-once = nm-applet --indicator & blueman-applet &
   exec-once = kwalletd6
   exec-once = sleep 3 && hyprctl dispatch exec "[workspace special:mail silent]" thunderbird
