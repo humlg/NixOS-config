cdf76c4d170ecc6087d833e07817d70d2c920a1f
David Huml <david.huml@email.cz>
2026-06-03 09:51:56 +0200

    nvidia: use new_feature driver package; bump flake inputs
    Pins nvidia.package to nvidiaPackages.new_feature for the latest
    feature branch driver. Also bumps nixpkgs, home-manager, astal,
    NUR, and zen-browser-flake to their current revisions.

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -6,6 +6,7 @@
     powerManagement.enable = true;
     open = false; # proprietary driver
     nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.new_feature;
   };
 
   boot.kernelParams = ["nvidia_drm.modeset=1" "nvidia_drm.fbdev=1"];
