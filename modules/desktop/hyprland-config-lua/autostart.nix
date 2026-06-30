a824a6b0759fe06559d6ebe9001bcdbfeb9017d5
David Huml <david.huml@email.cz>
2026-06-30 16:57:10 +0200

    fix(autostart): disable megasync auto-launch on startup

--- modules/desktop/hyprland-config-lua/autostart.nix
+++ modules/desktop/hyprland-config-lua/autostart.nix
@@ -9,7 +9,6 @@
     hl.exec_cmd("nm-applet --indicator & blueman-applet &")
     hl.exec_cmd("sleep 3 && hyprctl dispatch exec '[workspace special:mail silent]' thunderbird")
     hl.exec_cmd("sleep 3 && obsidian")
-    hl.exec_cmd("megasync")
     hl.exec_cmd("wl-paste --type text  --watch cliphist store")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
   end)
