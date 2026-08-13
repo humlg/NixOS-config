c8b304fb2a8bdd459b5f319e3c656da219fd0af4
David Huml <david.huml@email.cz>
2026-08-13 02:39:47 +0200

    Added support for encrypted vault

---
+++ modules/home/thunar.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [ gocryptfs zenity xfce.thunar ];
+
+  xdg.configFile."Thunar/uca.xml".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <actions>
+      <action>
+        <name>Mount Vault</name>
+        <command>sh -c 'gocryptfs -extpass "zenity --password --title=Vault" ~/.vault ~/Vault &amp;&amp; thunar ~/Vault'</command>
+        <description>Unlock encrypted vault</description>
+        <icon>folder-locked</icon>
+        <unique-id>vault-mount</unique-id>
+        <patterns>*</patterns>
+        <directories/>
+      </action>
+      <action>
+        <name>Unmount Vault</name>
+        <command>fusermount -u ~/Vault</command>
+        <description>Lock encrypted vault</description>
+        <icon>folder</icon>
+        <unique-id>vault-umount</unique-id>
+        <patterns>*</patterns>
+        <directories/>
+      </action>
+    </actions>
+'';
+}
