1786a88ba65549b7ffb52c927b3a7d579f466fb3
David Huml <david.huml@email.cz>
2026-09-03 14:31:19 +0200

    Added automatic garbadge collection and automatic store optimization

--- modules/bundles/general.nix
+++ modules/bundles/general.nix
@@ -16,6 +16,13 @@ in
     enable = lib.mkEnableOption "General always-installed user packages bundle";
   };
 
+  nix.optimise.automatic = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d"
+  };
+
   config = lib.mkIf cfg.enable {
     services.udiskie.enable = true;
     programs.git.enable = true;
